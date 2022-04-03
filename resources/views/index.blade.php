9aa4a244298a401b3d0e971d3879183edd5f276c
s-ike <shunt_i@yahoo.co.jp>
2022-04-03 14:52:44 +0900

    basic

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot name="title">
+        My BBS
+    </x-slot>
+
+    <h1>My BBS</h1>
+    <ul>
+        @forelse ($posts as $index => $post)
+            <li>
+                <a href="{{ route('posts.show', $index) }}">
+                    {{ $post }}</li>
+                </a>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+</x-layout>
